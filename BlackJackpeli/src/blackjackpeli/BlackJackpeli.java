81981df5e8583ee3ac2719dcd0a778b82f7f743b
pessiv <31436783+pessiv@users.noreply.github.com>
2017-09-19 13:13:59 +0300

    Update BlackJackpeli.java

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -1,6 +1,6 @@
 package blackjackpeli;
 public class BlackJackpeli {
     public static void main(String[] args) {
-        System.out.println("this is way too basic...");
+        System.out.println("this is way too basic....");
     }
 }
