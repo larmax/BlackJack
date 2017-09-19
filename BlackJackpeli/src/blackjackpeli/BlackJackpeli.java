bfec1b1e0e2072951168a053f29fc42ceaac936d
Vilppolahti Pessi <s1601401@student.businesscollege.fi>
2017-09-19 12:54:46 +0300

    'ok'

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -1,21 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjackpeli;
-
-/**
- *
- * @author s1601385
- */
 public class BlackJackpeli {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
-    
 }
