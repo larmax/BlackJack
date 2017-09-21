d8bb5de875e51416e2fc7b20525b614a9fe7bfe3
Vilppolahti Pessi <s1601401@student.businesscollege.fi>
2017-09-21 13:54:12 +0300

    '-'

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -1,6 +1,13 @@
 package blackjackpeli;
 public class BlackJackpeli {
     public static void main(String[] args) {
+        Korttipakka pakka = new Korttipakka();
         System.out.println("this is way too basic....");
+        for(int i = 1;i<=13;i++){
+            pakka.addkortti("hertta", i);
+            pakka.addkortti("ruutu", i);
+            pakka.addkortti("risti", i);
+            pakka.addkortti("pata", i);
+        }
     }
 }
--- BlackJackpeli/src/blackjackpeli/Korttipakka.java
+++ BlackJackpeli/src/blackjackpeli/Korttipakka.java
@@ -2,5 +2,8 @@ package blackjackpeli;
 import java.util.*;
 public class Korttipakka {
     ArrayList Korttipakka = new ArrayList();
-    
+    void addkortti(String maa, int arvo){
+        String kortti = maa+arvo;
+        Korttipakka.add(kortti);
+    }
 }
