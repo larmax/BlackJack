6e6e617a53f90c0eefb45b4a362527136f8cb17a
PV <pessiv@outlook.com>
2017-09-21 16:04:25 +0300

    'added-checkkortti()-to-korttipakka.java'

--- BlackJackpeli/src/blackjackpeli/Korttipakka.java
+++ BlackJackpeli/src/blackjackpeli/Korttipakka.java
@@ -9,7 +9,20 @@ public class Korttipakka {
     void remkortti(String kortti){
         String tkortti = "";
         for(int i = 0;i<=Korttipakka.size();i++){
-            
+            tkortti = Korttipakka.get(i).toString();
+            if(tkortti.equals(kortti)){
+                Korttipakka.remove(i);
+            }
         }
+    }
+    boolean checkkortti(String kortti){
+        String tkortti = "";
+        for(int i = 0;i<=Korttipakka.size();i++){
+            tkortti = Korttipakka.get(i).toString();
+            if(tkortti.equals(kortti)){
+                return true;
+            }
+        }
+        return false;
     }
 }
