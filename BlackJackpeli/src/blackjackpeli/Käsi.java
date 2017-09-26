a40fd18e03397ec5ed4d2e1b4c6287eaf5a74417
Pessiv <s1601401@student.businesscollege.fi>
2017-09-26 13:29:40 +0300

    'added-kasi.java'

---
+++ BlackJackpeli/src/blackjackpeli/Käsi.java
@@ -0,0 +1,26 @@
+
+package blackjackpeli;
+
+import java.util.*;
+
+
+
+public class Käsi {
+    
+ArrayList pelaajankäsi = new ArrayList();
+
+public void(String jotai){
+    
+}
+
+
+public static void otaKortti(ArrayList<String> Kortit){
+   
+    
+    
+}
+public static void selvitäSumma(){
+    
+}
+
+}
