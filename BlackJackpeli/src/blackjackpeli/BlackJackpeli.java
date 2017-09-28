fe94aaf3d555e4d469bec4f42a1772c1a78c946a
Miller Leon <s1601385@student.businesscollege.fi>
2017-09-28 13:35:43 +0300

    ---

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -1,7 +1,20 @@
+
 package blackjackpeli;
+import java.util.*;
 public class BlackJackpeli {
     public static void main(String[] args) {
         Korttipakka pakka = new Korttipakka();
+              Scanner pena = new Scanner(System.in);
+        
+        System.out.println("Haluatko kortin");
+        String vast = pena.nextLine();
+        if(vast.equals("joo")){
+            getKortit();
+        }else if(vast.equals("ei")){
+
+
+            System.out.println("ei sit");
+        }
         
         
     }
