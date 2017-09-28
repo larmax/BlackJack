02bbc2d2572f9fac8bc51b14605df81553d03ccb
Pessiv <s1601401@student.businesscollege.fi>
2017-09-28 14:00:06 +0300

    'main-class'

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -1,21 +1,22 @@
-
 package blackjackpeli;
 import java.util.*;
 public class BlackJackpeli {
     public static void main(String[] args) {
         Korttipakka pakka = new Korttipakka();
-              Scanner pena = new Scanner(System.in);
-        
-        System.out.println("Haluatko kortin");
-        String vast = pena.nextLine();
-        if(vast.equals("joo")){
-            getKortit();
-        }else if(vast.equals("ei")){
-
-
-            System.out.println("ei sit");
+        Scanner pena = new Scanner(System.in);
+        while(true){
+            System.out.println("haluatko pelata?");
+            String vast1 = pena.nextLine();
+            if(vast1.equals("joo")){
+                System.out.println("Haluatko kortin?");
+                String vast = pena.nextLine();
+                if(vast.equals("joo")){
+                }
+                else if(vast.equals("ei")){
+                    System.out.println("ei sit");
+                }
+            }
+            else{System.out.println("haist vittu sit. >:C");}
         }
-        
-        
     }
 }
