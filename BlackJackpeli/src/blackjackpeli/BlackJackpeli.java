6b9d637e49272f5335b2640a2aeb1492ac447796
pessiv <31436783+pessiv@users.noreply.github.com>
2017-10-10 13:42:30 +0300

    Update BlackJackpeli.java

--- BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
+++ BlackJackpeli/src/blackjackpeli/BlackJackpeli.java
@@ -10,26 +10,25 @@ public class BlackJackpeli {
         emanta ekasi = new emanta();
 
         Scanner pena = new Scanner(System.in);
-         System.out.println("haluatko pelata?");
+        System.out.println("haluatko pelata?");
         String vast1 = pena.nextLine();
-        
-        
+
         if (vast1.equals("joo")) {
             System.out.println("Monta pakkaa haluat peliin?");
-int vast2 = pena.nextInt();
-        Korttipakka pakka = new Korttipakka(vast2);
+            int vast2 = pena.nextInt();
+            Korttipakka pakka = new Korttipakka(vast2);
             while (true) {
-                int summa;
                 System.out.println("Haluatko kortin?");
-                String vast = pena.nextLine();
+                int summa;
+                String vast = pena.next();
                 if (vast.equals("joo")) {
                     pakka.sekoita();
                     Kortti jaettavakortti = pakka.getKortit().get(0);
                     //  pakka.remkortti(jaettavakortti);
                     pkasi.otaKortti(jaettavakortti);
-                    System.out.print(pkasi);
+                    System.out.println(pkasi);
                     summa = pkasi.selvitaSumma();
-                    System.out.println("Korttien summa on"+summa);
+                    System.out.println("Korttien summa on " + summa);
                     pisteet = summa;
                     if (summa > 21) {
                         System.out.println("hävisit. Parempi onnni ensi kerralla");
@@ -37,8 +36,6 @@ int vast2 = pena.nextInt();
                     }
                 } else if (vast.equals("ei")) {
 
-               
-              
                     int esumma = 0;
                     while (esumma <= 17) {
                         Kortti jaettavakortti = pakka.getKortit().get(0);
@@ -47,13 +44,13 @@ int vast2 = pena.nextInt();
                         ekasi.otaKortti(jaettavakortti);
                         esumma = ekasi.selvitäSumma();
                         Kortti ekortti = ekasi.getEmannanKortit();
-                    
+
                         System.out.println(ekortti);
-                    
+
                     }
                     System.out.println("ja niiden yhteisarvo on: ");
-                       System.out.println("nyt on sitten emännän vuoro:");
-                    System.out.println("emäntä sai seuraavat kortit: "+esumma);
+                    System.out.println("nyt on sitten emännän vuoro:");
+                    System.out.println("emäntä sai seuraavat kortit: " + esumma);
                     int psumma = pkasi.selvitaSumma();
                     if (esumma > 21 || psumma > esumma) {
                         System.out.println("voitit! :D");
